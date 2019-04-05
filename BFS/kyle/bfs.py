4d57d03e54209aa82c9711f6f51b4e201cb5f57e
kshammer <hammerschmiks@vcu.edu>
2019-04-04 22:15:58 -0500

    Coding in python 2 every day is poisoning my python 3 skills

--- BFS/kyle/bfs.py
+++ BFS/kyle/bfs.py
@@ -1,1 +1,22 @@
-print('I am a snake sssssss')
+import networkx as nx
+import queue
+
+
+def create_graph(nodes = 100, edges = 200):
+    g = nx.gnm_random_graph(nodes, edges)
+    return g
+
+def bfs():
+    g = create_graph(nodes = 15, edges = 25)
+    q = queue.Queue()
+    #g.add_node(-1)  I want to do some kind of find a node thing or something idk 
+    #g.add_edge(14, -1)
+
+    q.put(list(g.nodes()).pop(0))
+
+    while q.full():
+        print(q.get())
+
+if __name__ == "__main__":
+    bfs()
+    print("done !")
